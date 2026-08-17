46cfdb853205eca51925960cf4b00905a8153499
LukeNcCode <h1586130266@outlook.com>
2026-08-17 17:26:45 +0800

    data update

--- data/posts.js
+++ data/posts.js
@@ -1,14 +1,14 @@
 window.__BLOG_POSTS = [
     {
-        "id":  "lizi",
-        "status":  "draft",
-        "date":  "2026-08-10",
+        "id":  "thinking",
+        "status":  "published",
+        "date":  "2026-08-17",
         "tags":  [
                      "C语言",
                      "感悟"
                  ],
-        "title":  "我在寄存器里找到的确定性",
-        "summary":  "从 GPIO 点灯到直接操作寄存器：迷茫、烧板子，然后真的开始懂硬件。",
+        "title":  "随笔",
+        "summary":  "距离考研还有488天",
         "contentLen":  691
     }
 ];
